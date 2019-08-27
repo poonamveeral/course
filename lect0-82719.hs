1c043676492e69a497a0400bfedae478130b236c
Harley Eades <harley.eades@gmail.com>
2019-08-27 13:12:12 -0400

    Update to today\'s lecture notes.

--- lect0-82719.hs
+++ lect0-82719.hs
@@ -1,2 +1,26 @@
-test :: Int
-test = 2 + 2
+{-
+
+[x * 2 | x <- [1,2,3,4]]
+
+l = []
+for x in [1,2,3,4] {
+  for y in ['a','b','c'] {
+     add((x,y),l)
+  }
+}
+
+name :: Type
+name x1 x2 .... xi = body
+-}
+
+import Prelude
+
+l1 :: [Int]
+l1 = [1,2,3]
+
+p1 :: (Int,[Int])
+p1 = (1,l1)
+
+
+
+
